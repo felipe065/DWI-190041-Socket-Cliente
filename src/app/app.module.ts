a189797820f681ee8dca2eb9e4edf73ab8856cf3
FelipeParedes <felipeparedes238@gmail.com>
2022-06-24 13:30:43 -0500

    Primer commmit del servidor cliente-socket

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+const config: SocketIoConfig ={
+  url:environment.wsURL, options: {}
+}
+
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
